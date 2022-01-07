a1ce28034428b5345ed7d7d693940a3b419fd24c
MAbass <m.abassdiallo@gmail.com>
2022-01-07 11:49:50 +0000

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container bg-red-500 mx-auto text-center">
+      <h1 className="font-bold text-4xl">Hello world!!</h1>
     </div>
   );
 }
